d3f8a72bb35e957a8a7b4a09764fed559ecb279f
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 17:38:40 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,16 +6,16 @@ attr_accessor :path
 
   def initialize(path)
     @path = path
-    @path += "/*.mp3"
   end
 
   def files
+    @path += "/*.mp3"
     Dir[@path].collect {|el| el.split("/")[-1]}
     # binding.pry
   end
 
   def import
-    # @path += "/*.mp3"
+    @path += "/*.mp3"
     Dir[@path].each {|i| Song.new_by_filename(i)}
   end
 
