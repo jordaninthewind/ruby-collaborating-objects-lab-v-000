3affa480c1a64496c255faac97874f12c08b11c9
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 21:57:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,47 @@
-require 'pry'
+# require 'pry'
+#
+# class Artist
+# attr_accessor :name, :songs
+#
+# @@all = []
+#
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+#
+#   def add_song(song)
+#     @songs << song
+#   end
+#
+#   def save
+#     @@all << self
+#   end
+#
+#   def print_songs
+#     @songs.each { |el| puts el.name }
+#   end
+#
+#   def self.find(name)
+#     self.all.find {|i| i.name == name}
+#   end
+#
+#   def self.create(name)
+#     self.new(name).tap {|artist| artist.save}
+#   end
+#
+#   def self.find_or_create_by_name(artist)
+#     self.find(artist) ? self.find(artist) : self.create(artist)
+#   end
+# end
 
 class Artist
-attr_accessor :name, :songs
-
-@@all = []
+  @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
@@ -18,23 +56,24 @@ attr_accessor :name, :songs
     @songs << song
   end
 
-  def save
-    @@all << self
-  end
-
-  def print_songs
-    @songs.each { |el| puts el.name }
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.create(name)
   end
 
   def self.find(name)
-    self.all.find {|i| i.name == name}
+    self.all.find {|artist| artist.name == name }
   end
+
 
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
   end
 
-  def self.find_or_create_by_name(artist)
-    self.find(artist) ? self.find(artist) : self.create(artist)
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 end
