86a36fe8af8eb3e78f48b6789127c3202ad67551
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 21:31:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ attr_accessor :name, :artist
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self.name)
+    artist.add_song(self)
     # binding.pry
     # artist.save
   end
