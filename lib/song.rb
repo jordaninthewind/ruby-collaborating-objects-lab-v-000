a5c79cb64134ff02bec6e8e5ff31f4cc9df239ae
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 20:51:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ attr_accessor :name, :artist
   #   Artist.find_or_create_by_name(name)
   #   self.add_song()
   # end
-  
+
   def self.new_song(name)
     self.new(name)
   end
@@ -21,7 +21,7 @@ attr_accessor :name, :artist
     file = file_name.split(" - ")
     new_song = self.new_song(file[1])
     new_song.artist = Artist.find_or_create_by_name(file[0])
-    # binding.pry
+    binding.pry
     new_song
   end
 end
