2c8a3c61c1395549471ab60cbc4b919c98acea81
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 20:57:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,21 +7,19 @@ attr_accessor :name, :artist
     @name = name
   end
 
-  # def artist_name=(name)
-  #   # turns artist's name as string into new artist
-  #   Artist.find_or_create_by_name(name)
-  #   self.add_song()
-  # end
-
-  def self.new_song(name)
-    self.new(name)
+  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
+  # def self.new_song(name)
+  #   self.new(name)
+  # end
 
   def self.new_by_filename(file_name)
     file = file_name.split(" - ")
-    new_song = self.new_song(file[1])
+    new_song = self.new(file[1])
     new_song.artist = Artist.find_or_create_by_name(file[0])
-    binding.pry
     new_song
   end
 end
