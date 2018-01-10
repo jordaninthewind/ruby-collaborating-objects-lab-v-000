a713d9f45a302335ed476d8a6da970a195903522
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 18:02:47 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,9 @@ attr_accessor :path
   end
 
   def files
-    Dir[(@path += "/*.mp3")].map { |el| el.split("/").last }
+    Dir[(@path += "/*.mp3")].map do |el|
+      el.split("/").last
+    end
   end
 
   def import
