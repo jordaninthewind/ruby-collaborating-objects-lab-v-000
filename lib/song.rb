3e0d6700ce61f416133decb37a6ae1a483dfc321
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 20:09:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,14 +10,14 @@ attr_accessor :name, :artist
   def self.new_by_filename(file_name)
     file = file_name.split(" - ")
     new_song = Song.new(file[1])
-    new_song.artist = Artist.find_or_create_by_name(file[0])
+    new_song.artist_name = file[0]
     binding.pry
     # new_song.artist.songs << new_song
     new_song
   end
 
   def artist_name=(name)
-
+    Artist.find_or_create_by_name(name)
   end
 
 end
