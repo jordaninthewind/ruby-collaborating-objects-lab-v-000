8cae273364ef9a6f5fbe1a06f30839b9c5a32f4b
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 21:59:23 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,41 +1,21 @@
-# require 'pry'
-#
-# class MP3Importer
-#
-# attr_accessor :path
-#
-#   def initialize(path)
-#     @path = path
-#     @files = []
-#   end
-#
-#   def files
-#     @path += "/*.mp3" unless @path.split("/").last == "*.mp3"
-#     Dir[@path].each { |el| @files << el.split("/").last }
-#     @files
-#   end
-#
-#   def import
-#     @files.each do |i|
-#       Song.new_by_filename(i)
-#     end
-#   end
-# end
+require 'pry'
 
 class MP3Importer
-  attr_reader :path
+
+attr_accessor :path
 
   def initialize(path)
     @path = path
+    @files = []
   end
 
   def files
-    @path += "/*.mp3" unless @path.split("/").last == "*.mp3"
-    Dir[@path].each { |el| @files << el.split("/").last }
-    @files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
-
+  
   def import
-    files.each{|f| Song.new_by_filename(f)}
+    @files.each do |i|
+      Song.new_by_filename(i)
+    end
   end
 end
