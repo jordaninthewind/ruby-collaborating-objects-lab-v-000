d80581172246ae4533e50090c0ad53f53b6cfcf0
Jordan Kline <jordan.kline@gmail.com>
2018-01-10 16:39:38 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+require 'pry'
+
+class Artist
+attr_accessor :name, :songs
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    self.songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    self.songs.each do |el|
+      puts el.name
+    end
+  end
+
+  def self.find_or_create_by_name(artist)
+    
+    if self.all.any? {|i| i.name == artist }
+      artist
+    else
+      Artist.new(artist)
+    end
+  end
+end
